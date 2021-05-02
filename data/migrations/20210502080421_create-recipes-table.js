976ceedae9ef0e7c44f328987bd70f338695afef
unknown <Katherine2bk@gmail.com>
2021-05-02 01:30:03 -0700

    set up table for recipes, did migrate:latest... received database, hope everything continues successfully... checking now

--- data/migrations/20210502080421_create-recipes-table.js
+++ data/migrations/20210502080421_create-recipes-table.js
@@ -1,8 +1,39 @@
 
 exports.up = function(knex) {
-  
+  return knex.schema.createTable('recipes', (table) => {
+      table.increments('id')
+      table.string('name', 255).notNullable().unique()
+  })
+  .createTable('ingredients', (table) => {
+      table.increments()
+      table.string('ingredientName', 255).notNullable()
+      table.integer('quantity').notNullable()
+      table.integer('recipe_id')
+      .unsigned()
+      .notNullable()
+      .references('id')
+      .inTable('recipes')
+  })
+  .createTable('instructions', (table) => {
+      table.increments()
+      table.integer('step_number')
+      .unsigned()
+      .notNullable()
+      table.text('step_instructions').notNullable()
+      table.integer('recipe_id')
+      .unsigned()
+      .notNullable()
+      .references('id')
+      .inTable('recipes')
+      .onDelete('CASCADE')
+      .onUpdate('CASCADE')
+  })
+
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema
+    .dropTableIfExists('instructions')
+    .dropTableIfExists('ingredients')
+    .dropTableIfExists('recipes')
 };
